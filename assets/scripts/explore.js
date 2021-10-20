0a7a53952149ce9d4d0505591d8fadad7ab3dcd4
Jerry Zhang <jerrychzhang@gmail.com>
2021-10-20 14:34:02 -0700

    Explore: Finish speech synthesis

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,41 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  let synth = window.speechSynthesis;
+  let voiceSelect = document.getElementById('voice-select');
+  let textToSpeak = document.getElementById('text-to-speak');
+  let speakButton = document.querySelector('button');
+  let speakImg = document.querySelector('img');
+  let voices = [];
+
+  function populateVoiceList() {
+    voices = synth.getVoices();
+
+    for (var i = 0; i < voices.length; i++) {
+      let option = document.createElement('option');
+      option.textContent = voices[i].name;
+      voiceSelect.appendChild(option);
+    }
+  }
+
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+
+  speakButton.addEventListener('click', function() {
+    let utterance = new SpeechSynthesisUtterance(textToSpeak.value);
+    let voiceName = voiceSelect.value;
+    for (const voice in voices) {
+      if (voices[voice].name == voiceName) {
+        utterance.voice = voices[voice];
+      }
+    }
+    utterance.addEventListener('start', function() {
+      speakImg.setAttribute('src', 'assets/images/smiling-open.png');
+    });
+    utterance.addEventListener('end', function() {
+      speakImg.setAttribute('src', 'assets/images/smiling.png');
+    });
+    synth.speak(utterance);
+  });
+
+
 }
